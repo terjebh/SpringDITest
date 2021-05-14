6f10760a0d17cd4e565c649b443d486a161a954b
terjebh <terje@itfakultetet.no>
2021-05-15 01:31:46 +0200

    DI

---
+++ src/test/java/com/noderia/Spring/DI/Test/SpringDiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noderia.Spring.DI.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
